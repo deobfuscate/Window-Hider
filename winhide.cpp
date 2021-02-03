0af37997b9bf9e47498bd52dd2a914f605edd972
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-02-02 21:41:10 -0800

    Add second hotkey

--- winhide.cpp
+++ winhide.cpp
@@ -13,16 +13,30 @@ void main() {
     else {
         printf("Failed to register hotkey\n");
     }
+    if (RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, 0x43))  //0x42 = 'b'
+    {
+        printf("Hotkey ALT+c with MOD_NOREPEAT flag\n");
+    }
+    else {
+        printf("Failed to register hotkey\n");
+    }
 
     while (GetMessage(&msg, NULL, 0, 0) != 0)
     {
-        if (msg.message == WM_HOTKEY)
+        if (msg.message == WM_HOTKEY && msg.wParam == 1)
         {
             HWND hw = GetForegroundWindow();
             int bufsize = GetWindowTextLength(hw) + 1;
             char title[BUFSIZ];
             GetWindowTextA(hw, title, bufsize);
-            cout << title << endl;
+            cout << "The window: \"" << title << "\" was hidden " << hw << endl;
+            windows.push_front(hw);
+        }
+        if (msg.message == WM_HOTKEY && msg.wParam == 2)
+        {
+            if (!windows.empty()) {
+                windows.pop_front();
+            }
         }
     }
 }
