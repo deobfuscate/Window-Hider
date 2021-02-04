443b28d4ebd9233caee7ff4ca4a4a03fe0603c2e
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-02-03 22:53:47 -0800

    Refactor hotkeys

--- winhide.cpp
+++ winhide.cpp
@@ -6,25 +6,16 @@ using namespace std;
 void main() {
     MSG msg = { 0 };
     list<HWND> windows;
-    if (RegisterHotKey(NULL, 1, MOD_ALT | MOD_NOREPEAT, 0x42))  //0x42 = 'b'
-    {
+    if (RegisterHotKey(NULL, 1, MOD_ALT | MOD_NOREPEAT, 0x42) && // ALT+b
+        RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, 0x43)) { // ALT+c
         printf("Hotkey ALT+b with MOD_NOREPEAT flag\n");
     }
     else {
         printf("Failed to register hotkey\n");
     }
-    if (RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, 0x43))  //0x42 = 'b'
-    {
-        printf("Hotkey ALT+c with MOD_NOREPEAT flag\n");
-    }
-    else {
-        printf("Failed to register hotkey\n");
-    }
 
-    while (GetMessage(&msg, NULL, 0, 0) != 0)
-    {
-        if (msg.message == WM_HOTKEY && msg.wParam == 1)
-        {
+    while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        if (msg.message == WM_HOTKEY && msg.wParam == 1) { // ALT+b
             HWND hw = GetForegroundWindow();
             int bufsize = GetWindowTextLength(hw) + 1;
             char title[BUFSIZ];
@@ -32,8 +23,7 @@ void main() {
             cout << "The window: \"" << title << "\" was hidden " << hw << endl;
             windows.push_front(hw);
         }
-        if (msg.message == WM_HOTKEY && msg.wParam == 2)
-        {
+        if (msg.message == WM_HOTKEY && msg.wParam == 2) { // ALT+c
             if (!windows.empty()) {
                 windows.pop_front();
             }
