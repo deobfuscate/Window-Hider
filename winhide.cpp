ba8ebc23e198fb27609029121c80ed424959505a
Dan <33081566+deobfuscate@users.noreply.github.com>
2021-04-05 18:45:48 -0700

    Fix error false positive

--- winhide.cpp
+++ winhide.cpp
@@ -47,29 +47,25 @@ void main() {
     char hide_key = KEY_B;
     char show_key = KEY_C;
     char cwd[BUFSIZ];
+
     GetCurrentDirectoryA(BUFSIZ, cwd);
     string ini_path = string(cwd) + "\\winhidecfg.ini";
     int start_hidden_ini = GetPrivateProfileIntA("Settings", "StartHidden", 0, ini_path.c_str());
-    if (GetLastError() != 0)
-        cerr << "Unable to read configuration file winhidecfg.ini, using defaults1" << endl;
+    auto last_error = GetLastError();
+    if (last_error != 0 && last_error != ERROR_MORE_DATA)
+        cerr << "Unable to read configuration file winhidecfg.ini, using defaults" << endl;
     else
         start_hidden = start_hidden_ini;
 
     LPSTR hide_key_ini = new CHAR[BUFSIZ];
     char ini_status = GetPrivateProfileStringA("Settings", "HideKey", LPCSTR(KEY_B), hide_key_ini, 2, ini_path.c_str());
-
-    if (GetLastError() != 0)
-        cerr << "Unable to read configuration file winhidecfg.ini, using defaults2" << endl;
+    last_error = GetLastError();
+    if (last_error != 0 && last_error != ERROR_MORE_DATA)
+        cerr << "Unable to read configuration file winhidecfg.ini, using defaults" << endl;
     else
         hide_key = hide_key_ini[0];
 
-    LPSTR show_key_ini = new CHAR[BUFSIZ];
-    ini_status = GetPrivateProfileStringA("Settings", "ShowKey", LPCSTR(KEY_C), show_key_ini, 2, ini_path.c_str());
 
-    if (GetLastError() != 0)
-        cerr << "Unable to read configuration file winhidecfg.ini, using defaults3" << endl;
-    else
-        show_key = show_key_ini[0];
 
     if (start_hidden != false) {
         HWND console = GetConsoleWindow();
